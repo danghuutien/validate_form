bfe092a92ed607df964fb5e2ae85ac242104971c
danghuutien <danghuutientk123@gmail.com>
2022-10-26 09:17:04 +0700

    Update callAPI.js

--- assets/js/callAPI.js
+++ assets/js/callAPI.js
@@ -5,18 +5,18 @@ window.onload = () => {
 const notification = document.getElementById("notification");
 
 Validator({
-  form: "#test_validation",
-  errorSelector: ".form-message",
-  rules: [
-    isRequired('input[name = "Marital"]'),
-    isRequired("#age"),
-    isAge("#age"),
-    isRequired("#Gender"),
-    isRequired("#Phone"),
-    minLength("#Phone"),
-    isRequired("#Email"),
-    isEmail("#Email"),
-    isRequired("#Marital_status"),
+    form: "#test_validation",
+    errorSelector: ".form-message",
+    rules: [
+        isRequired('input[name = "Marital"]'),
+        isRequired("#age"),
+        isAge("#age"),
+        isRequired("#Gender"),
+        isRequired("#Phone"),
+        minLength("#Phone"),
+        isRequired("#Email"),
+        isEmail("#Email"),
+        isRequired("#Marital_status"),
   ],
   onSubmit: (data = null) => {
     // console.log(data);
@@ -25,55 +25,51 @@ Validator({
       renderData.innerHTML =
         '<h1 style = "font-size:15px;color:red;"> Bạn phải nhập đầy đủ thông tin </h1>';
     } else {
-      Object.keys(data).forEach((key) => {
-        if (data[key] == "other") {
-          data[key] = data.Marital_status;
-        }
-      });
-      delete data.Marital_status;
-      createUser(data);
-
-      async function createUser(data) {
-        try {
-          const response = await fetch(
-            "https://634fcc2978563c1d82b00708.mockapi.io/api/User",
-            {
-              method: "POST", // or 'PUT'
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(data),
+        Object.keys(data).forEach((key) => {
+            if (data[key] == "other") {
+            data[key] = data.Marital_status;
             }
-          );
-
-          // console.log(response);
-          if (response.ok) {
-            // console.log(111111)
-            hienThi();
-            notification.querySelector("#success").innerText =
-              "Bạn đã nhập thành công";
-            notification.querySelector("#success").style.display = "inline";
-            // document.getElementById("error").innerHTML  = '<span style="color:green">Bạn đã nhập thành công</span>';
-            setTimeout(() => {
-              notification.querySelector("#success").style.display = "none";
-            }, 3000);
-          } else {
-            throw new Error(`${response.status} - ${response.statusText}`);
-          }
-        } catch (error) {
-          notification.querySelector("#err").innerText = "lỗi: " + error;
-          notification.querySelector("#err").style.display = "inline";
-          // document.getElementById("error").style.color = 'red'
-          // document.getElementById("error").innerHTML = 'lỗi: ' + error
-          setTimeout(() => {
-            notification.querySelector("#err").style.display = "none";
-          }, 3000);
-          // console.log(11111)
-        }
-      }
+        });
+        delete data.Marital_status;
+        createUser(data);
     }
   },
 });
+
+async function createUser(data) {
+    try {
+    const response = await fetch(
+        "https://634fcc2978563c1d82b00708.mockapi.io/api/User",
+        {
+        method: "POST", // or 'PUT'
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        }
+    );
+
+    if (response.ok) {
+        hienThi();
+        notification.querySelector("#success").innerText =
+        "Bạn đã nhập thành công";
+        notification.querySelector("#success").style.display = "inline";
+        // document.getElementById("error").innerHTML  = '<span style="color:green">Bạn đã nhập thành công</span>';
+        setTimeout(() => {
+        notification.querySelector("#success").style.display = "none";
+        }, 3000);
+    } else {
+        throw new Error(`${response.status} - ${response.statusText}`);
+    }
+    } catch (error) {
+    notification.querySelector("#err").innerText = "lỗi: " + error;
+    notification.querySelector("#err").style.display = "inline";
+    setTimeout(() => {
+        notification.querySelector("#err").style.display = "none";
+    }, 3000);
+    
+    }
+}
 
 async function hienThi() {
   const renderData = document.getElementById("data");
@@ -94,7 +90,8 @@ async function hienThi() {
           }
         });
 
-        listDatas += `</table>`+
+        listDatas +=
+          `</table>` +
           `<button onclick = 'deleteUser(${user["id"]}) '>xóa </button>` +
           `<button onclick = 'updateUser(${user["id"]}) '><label for="scroll">sửa</label></button>`;
         // renderData.querySelector('table').innerHTML = listDatas
@@ -111,4 +108,3 @@ async function hienThi() {
     }, 3000);
   }
 }
-
